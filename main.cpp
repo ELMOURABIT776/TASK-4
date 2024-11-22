082c6f206798129221f729ecfe61c5fe4fa12c35
ELMOURABIT776 <elnadjah.elmourabit.abderrahim@gmail.com>
2024-11-22 09:14:49 +0100

    DONE

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2, result;
+    int choice;
+
+    // Display the menu
+    cout << "Menu-Driven Calculator\n";
+    cout << "1. Addition\n";
+    cout << "2. Subtraction\n";
+    cout << "3. Multiplication\n";
+    cout << "4. Division\n";
+    cout << "Enter your choice (1-4): ";
+    cin >> choice;
+
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+
+    switch (choice) {
+        case 1:
+            result = num1 + num2;
+        cout << "Result: " << result << endl;
+        break;
+
+        case 2:
+            result = num1 - num2;
+        cout << "Result: " << result << endl;
+        break;
+
+        case 3:
+            result = num1 * num2;
+        cout << "Result: " << result << endl;
+        break;
+
+        case 4:
+            if (num2 != 0) {
+                result = num1 / num2;
+                cout << "Result: " << result << endl;
+            } else {
+                cout << "Error: Division by zero is not allowed.\n";
+            }
+        break;
+
+        default:
+            cout << "Invalid choice! Please select an option from 1 to 4.\n";
+    }
+
+    return 0;
+}
